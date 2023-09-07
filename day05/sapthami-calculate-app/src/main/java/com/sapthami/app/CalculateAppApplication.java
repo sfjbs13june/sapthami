028c1d7f60b22e51a35f09b1e34df6bca31d128e
sfjbs <sapthami@gmail.com>
2023-09-07 05:54:04 +0000

    day05 committed

---
+++ day05/sapthami-calculate-app/src/main/java/com/sapthami/app/CalculateAppApplication.java
@@ -0,0 +1,13 @@
+package com.sapthami.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculateAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculateAppApplication.class, args);
+	}
+
+}
---
+++ day05/sapthami-calculate-app/src/test/java/com/sapthami/app/CalculateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sapthami.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculateAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
