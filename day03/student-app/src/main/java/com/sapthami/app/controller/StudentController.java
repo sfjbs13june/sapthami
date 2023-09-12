6fcebe3d510c03418bc13fec49cf634b5f6e76e3
sfjbs <sapthami@gmail.com>
2023-09-12 09:53:33 +0000

    Student Application

--- day03/student-app/src/main/java/com/sapthami/app/controller/StudentController.java
+++ day03/student-app/src/main/java/com/sapthami/app/controller/StudentController.java
@@ -1,6 +1,8 @@
 package com.sapthami.app.controller;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +13,5 @@ public class StudentController {
     public String getStudentDetail(){
         return "student1";
     }
+
 }
