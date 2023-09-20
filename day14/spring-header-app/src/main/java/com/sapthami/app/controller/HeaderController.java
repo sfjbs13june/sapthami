747e97be88b186795aa80ea38437318c00b95e8a
sfjbs <sapthami@gmail.com>
2023-09-20 08:22:53 +0000

    demo spring-headers

---
+++ day14/spring-header-app/src/main/java/com/sapthami/app/controller/HeaderController.java
@@ -0,0 +1,20 @@
+package com.sapthami.app.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HeaderController {
+    @PostMapping("/send/data")
+    public ResponseEntity sendData(@RequestHeader("sessionid") String sessionid) {
+
+        if (sessionid == null) {
+            return new ResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+        System.out.println(sessionid);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+}
